8eba87532f08c7ea2456e234b30f460d96a7165a
Andrae <andra3b@gmail.com>
2025-10-17 16:27:21 +0100

    Initial commit

---
+++ Build.lua
@@ -0,0 +1,27 @@
+local Seam = require("Seam.Seam")
+
+local BuildTypeEnum = Enum.Create({
+	Release = 1,
+	Debug = 2
+})
+
+Seam.Options.BuildType = {
+	DataType = Enum.OptionDataType.Enum,
+	Default = BuildTypeEnum.Release,
+	Enum = BuildTypeEnum
+}
+
+Seam.UpdateOptions(arg)
+
+local BuildTypeAdditionalOptions = {
+	[BuildTypeEnum.Release] = "-Wall -std=c11 -O2",
+	[BuildTypeEnum.Debug] = "-Wall -std=c11 -g -Og -D DEBUG"
+}
+
+local MainApplication = Application.Create("Main")
+
+MainApplication:AddSource("./Application/Sources/", nil, "*.c")
+
+MainApplication.AdditionalCompilerOptions = BuildTypeAdditionalOptions[Seam.Options.BuildType.Value]
+
+MainApplication:Build()
